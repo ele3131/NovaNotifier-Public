03e9731764a2a102d487a2f506bd2e53579b3007
Michel <michelmarques1994@hotmail.com>
2020-09-13 20:15:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-from NovaNotifier2 import *
 
-player = Login()
-config = Read_settings_file()
-items = Read_id_file().items
-Read_medians_file(items)
-Network().items_network(items, player.cookies[0])
-Medians_history(items, config.settings)
-NovaNotifier(items)
