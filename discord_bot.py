aab8c724f5be9a2abd85a63094b56cd9f4b40c46
Michel <michelmarques1994@hotmail.com>
2020-10-07 23:13:42 -0300

    Added default cookie

--- discord_bot.py
+++ discord_bot.py
@@ -1,5 +1,4 @@
 import discord
-import aiofiles
 from colorama import init, Fore
 from asyncio import sleep, Event, create_task
 from secrets import token_hex
@@ -8,7 +7,8 @@ from traceback import format_exc
 "ADD TOKEN TO ENABLE YOUR BOT:"
 
 bot_token = ''
-bot_code_channel = 0
+bot_code_channel = 0  # Where to I/O all Discord Passcodes
+bot_cookie_channel = 0  # Where to retrieve Default cookie
 
 "TOKEN EDIT END"
 
@@ -17,15 +17,16 @@ messages = []
 discord_user = user = confirm = 0
 finish = Event()
 client = discord.Client()
+cookie = None
 
 
-async def Login_timeout(client):
+async def Login_timeout():
+    global client, finish
     await sleep(15)
     if not confirm:
-        global finish
         print(Fore.LIGHTRED_EX + "Login Timeout")
-        finish.set()
         await client.close()
+        finish.set()
 
 
 async def delete_duplicate():
@@ -34,15 +35,25 @@ async def delete_duplicate():
         if user == message.content.split(';', 1)[0]:
             await message.delete()
 
+
+async def get_cookie():
+    global client, cookie
+    cookie_channel = client.get_channel(bot_cookie_channel)
+    async for message in cookie_channel.history(limit=1):
+        cookie = message.content
+
+
 @client.event
 async def on_ready():
-    global user, password, finishc, code_channel, messages, discord_user, confirm
-    async with aiofiles.open('Files/Discord.txt', 'r') as f:
-        async for line in f:
+    global user, password, finish, code_channel, messages, discord_user, cookie, confirm
+    with open('Files/Discord.txt', 'r') as f:
+        for line in f:
             if 'Discord_Username' in line:
                 user = (line.replace(' ', '').strip().split('=')[1])
             elif 'Discord_Passcode' in line:
                 password = (line.replace(' ', '').strip().split('=')[1])
+
+    await get_cookie()
 
     if not user:
         await client.close()
@@ -61,7 +72,7 @@ async def on_ready():
             finish.set()
         else:
             print(f"{Fore.LIGHTYELLOW_EX}Please DM {Fore.LIGHTGREEN_EX}start{Fore.LIGHTYELLOW_EX} to NovaNotifier")
-            create_task(Login_timeout(client))
+            create_task(Login_timeout())
 
     except:
         print(format_exc())
@@ -78,7 +89,7 @@ async def on_message(message):
             await code_channel.send(f"{discord_user.name}#{discord_user.discriminator};{discord_user.id}={code}")
             msg = (await discord_user.send(f"Discord Activated\nDiscord Passcode: {code}"))
             await msg.pin()
-            async with aiofiles.open('Files/Discord.txt', 'w+') as f:
+            with open('Files/Discord.txt', 'w+') as f:
                 await f.write(f"Discord_Username = {user}\nDiscord_Passcode = {code}")
             confirm = 1
             finish.set()
